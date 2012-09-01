7883117798babb911117b1b8721a4ebae463cbdc
adammurphy <adam.b.murphy@gmail.com>
2012-09-01 15:07:04 +0100

    another

--- GitHibTestProject3/src/com/gittest/main2/Main.java
+++ GitHibTestProject3/src/com/gittest/main2/Main.java
@@ -3,6 +3,6 @@ package com.gittest.main2;
 public class Main {
 
 	public static void main(String ... args){ 
-		System.out.println("A different main12345");
+		System.out.println("A different main12345 6");
 	}
 }
