97d70ddd9b8e15e86b4efc36dcd45fe0d8a98746
adammurphy <adam.b.murphy@gmail.com>
2012-09-01 14:17:45 +0100

    changed println message

--- GitHibTestProject3/src/com/gittest/main2/Main2.java
+++ GitHibTestProject3/src/com/gittest/main2/Main2.java
@@ -3,6 +3,6 @@ package com.gittest.main2;
 public class Main2 {
 
 	public static void main(String ... args){ 
-		System.out.println("Git is cool2 staged");
+		System.out.println("Git is cool staged");
 	}
 }
