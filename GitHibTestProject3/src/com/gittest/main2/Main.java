106136c0c80a7a3a1194b4cb9e11e1912850b55b
adammurphy <adam.b.murphy@gmail.com>
2012-09-01 15:25:21 +0100

    branch commit

--- GitHibTestProject3/src/com/gittest/main2/Main.java
+++ GitHibTestProject3/src/com/gittest/main2/Main.java
@@ -3,6 +3,6 @@ package com.gittest.main2;
 public class Main {
 
 	public static void main(String ... args){ 
-		System.out.println("A different mains12 3 another");
+		System.out.println("A different mains12 3 another NONE branch");
 	}
 }
