189aa8bc90940196d4eddd7dab68133512e16381
adammurphy <adam.b.murphy@gmail.com>
2012-09-01 15:28:18 +0100

    123

--- GitHibTestProject3/src/com/gittest/main2/Main.java
+++ GitHibTestProject3/src/com/gittest/main2/Main.java
@@ -3,6 +3,6 @@ package com.gittest.main2;
 public class Main {
 
 	public static void main(String ... args){ 
-		System.out.println("A different mains12 3 another NONE branch");
+		System.out.println("A different mains12 3 another NONE branch 123");
 	}
 }
